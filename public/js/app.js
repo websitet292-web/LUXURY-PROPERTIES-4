415d6ad3ed420099b69c34611259345bb8bb0df2
websitet292-web <websitet292@gmail.com>
2026-09-24 20:04:27 +0530

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1962,7 +1962,7 @@ function renderUserLogin() {
 </div>
 
       <div class="relative z-10 luxury-card border border-amber-400/30 w-full max-w-sm p-5 sm:p-6 bg-[#0b0e14]/85 shadow-2xl backdrop-blur-xl rounded-3xl">
-
+      
         <!-- PREMIUM LOGO -->
         <div class="text-center mb-8">
 
